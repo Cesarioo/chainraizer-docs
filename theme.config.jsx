3ae3a3820c4b7fff13d368d73c1dcf8dda03dbfe
Oscar Mairey <o@mairey.net>
2025-01-13 17:49:43 +0400

    init

--- theme.config.jsx
+++ theme.config.jsx
@@ -3,25 +3,56 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default {
-  logo: <b>Documentation</b>,
+  logo: (
+    <div className="flex items-center">
+      <Image 
+        src="/logo.png" 
+        alt="ChainRaizer Logo" 
+        width={100} 
+        height={150}
+        className="h-full w-auto"
+      />
+    </div>
+  ),
   project: {
     link: 'https://github.com/Cesarioo/chainraizer-docs'
   },
   docsRepositoryBase: 'https://github.com/Cesarioo/chainraizer-docs',
-  footer: {
-    text: `MIT ${new Date().getFullYear()} © Your Name.`
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – ChainRaizer Docs'
+    }
   },
+  footer: {
+    text: `MIT ${new Date().getFullYear()} © ChainRaizer.`
+  },
+ 
+
+  navigation: {
+    prev: true,
+    next: true
+  },
+  darkMode: true,
+ 
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content="Documentation site built with Nextra" />
-      <title>My Nextra Docs</title>
+      <title>ChainRaizer Docs</title>
     </>
   ),
   primaryHue: {
     dark: 204,
     light: 212
   },
+ 
+
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+    autoCollapse: true,
+    toggleButton: true
+  },
+ 
   font: false,
   useNextSeoProps() {
     return {
