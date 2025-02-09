c8aeb9cb5973e161a85bf1344e481c0e3eca97c2
Oscar Mairey <o@mairey.net>
2025-02-09 21:11:14 +0400

    added quizz

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,7 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
 import 'nextra-theme-docs/style.css'
 import { Inter } from 'next/font/google'
-import FloatingButton from '@/components/FloatingButton'
+import FloatingButton from '../components/FloatingButton'
 
 const inter = Inter({ subsets: ['latin'] })
 
